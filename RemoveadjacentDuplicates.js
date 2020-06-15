4345842d53c69e320fb890ee5cc26551b2d46f60
Manuel Rios Martinez <60763319+mrios5@users.noreply.github.com>
2020-06-14 21:23:39 -0500

    Add files via upload

---
+++ RemoveadjacentDuplicates.js
@@ -0,0 +1,13 @@
+function ChasquidoDeThanos(Universo){
+    let flagUniverso = []
+    let UniversoSize = Universo.length
+
+    for(let i = UniversoSize-1; i>=0; i--)
+
+        if(flagUniverso.indexOf(Universo[i]) === -1){
+            flagUniverso.push(Universo[i])
+        }
+        return flagUniverso;
+}
+let Universo = [1,2,3,12,4,5,14,56,61,2,45,67,1,2nod]
+console.log(ChasquidoDeThanos(Universo))
